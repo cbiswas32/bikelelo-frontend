a9951bbcf77028c4846b53f6ac04fdef658bdbdb
Chinmoy <chinmoy.biswas@streebo.com>
2026-03-24 20:49:03 +0530

    feat: add SVG icons for users and Vite, create Navbar component, and set up initial project structure with Tailwind CSS and custom styles

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='flex justify-center'>
+      <h1>Bike lelo</h1>
+    </div>
+    
+    </>
+  )
+}
+
+export default App
